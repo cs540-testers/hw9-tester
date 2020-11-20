7cda5fe18d99db444c492295158d0d96b6a9358a
Stephen Jasina <stephenjasina@gmail.com>
2020-11-20 04:34:02 -0600

    Revamp tests and formatting

--- test.py
+++ test.py
@@ -1,29 +1,32 @@
 __author__ = 'cs540-testers'
 __credits__ = ['Harrison Clark', 'Stephen Jasina', 'Saurabh Kulkarni',
 		'Alex Moon']
-version = 'v0.1' 
-# TODO: set this to 1.0 once you've independently confirmed output vals
+__version__ = '1.0'
 
-import unittest
+# Suppress GPU warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import contextlib
 import io
-import intro_keras
-import sys
+import unittest
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
+from intro_keras import get_dataset, print_stats, build_model, train_model, \
+        evaluate_model, predict_label
 
 class TestIntroKeras(unittest.TestCase):
-
-
     def test_get_dataset(self):
-        (train_images, train_labels) = intro_keras.get_dataset()
-        (test_images, test_labels) = intro_keras.get_dataset(training=False)
+        train_images, train_labels = get_dataset()
+        test_images, test_labels = get_dataset(False)
 
-        self.assertEqual(type(train_images), np.ndarray)
-        self.assertEqual(type(train_labels), np.ndarray)
-        self.assertEqual(type(test_images), np.ndarray)
-        self.assertEqual(type(test_labels), np.ndarray)
-        self.assertEqual(type(train_labels[0]), np.uint8)
-        self.assertEqual(type(train_images[0]), np.ndarray)
+        self.assertIsInstance(train_images, np.ndarray)
+        self.assertIsInstance(train_labels, np.ndarray)
+        self.assertIsInstance(test_images, np.ndarray)
+        self.assertIsInstance(test_labels, np.ndarray)
+        self.assertIsInstance(train_labels[0], np.uint8)
+        self.assertIsInstance(train_images[0], np.ndarray)
         self.assertEqual(train_images.shape, (60000, 28, 28))
         self.assertEqual(train_labels.shape, (60000,))
         self.assertEqual(test_images.shape, (10000, 28, 28))
@@ -34,126 +37,159 @@ class TestIntroKeras(unittest.TestCase):
 
 
     def test_print_stats(self):
-        (train_images, train_labels) = intro_keras.get_dataset()
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-        intro_keras.print_stats(train_images, train_labels)
-        output = capturedOutput.getvalue()
-        sys.stdout = sys.__stdout__
-        expectedoutput = "60000\n"\
-        + "28x28\n"\
-        + "0. Zero - 5923\n"\
-        + "1. One - 6742\n"\
-        + "2. Two - 5958\n"\
-        + "3. Three - 6131\n"\
-        + "4. Four - 5842\n"\
-        + "5. Five - 5421\n"\
-        + "6. Six - 5918\n"\
-        + "7. Seven - 6265\n"\
-        + "8. Eight - 5851\n"\
-        + "9. Nine - 5949\n"
-        self.assertEqual(output, expectedoutput)
+        train_images, train_labels = get_dataset()
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            print_stats(train_images, train_labels)
+        expectedoutput = '60000\n'\
+                + '28x28\n'\
+                + '0. Zero - 5923\n'\
+                + '1. One - 6742\n'\
+                + '2. Two - 5958\n'\
+                + '3. Three - 6131\n'\
+                + '4. Four - 5842\n'\
+                + '5. Five - 5421\n'\
+                + '6. Six - 5918\n'\
+                + '7. Seven - 6265\n'\
+                + '8. Eight - 5851\n'\
+                + '9. Nine - 5949\n'
+        self.assertEqual(captured_output.getvalue(), expectedoutput)
+
 
     def test_build_model(self):
-        # TODO: use model.get_layer to check layers?
-        # Kinda checked in train_model
-        model = intro_keras.build_model()
-        self.assertEqual(model.__class__.__name__, "Sequential")
-        self.assertEqual(model.loss.__class__.__name__, "SparseCategoricalCrossentropy")
-        self.assertEqual(model.optimizer.__class__.__name__, "SGD")
-        self.assertEqual(model.metrics_names, []) #should be empty before fit() is called
+        model = build_model()
 
+        # Check model type
+        self.assertIsInstance(model,
+                tf.python.keras.engine.sequential.Sequential)
+        self.assertIsInstance(model.loss,
+                tf.python.keras.losses.SparseCategoricalCrossentropy)
+        self.assertIsInstance(model.optimizer,
+                tf.python.keras.optimizer_v2.gradient_descent.SGD)
 
-    
-    def test_train_model(self):
-        (train_images, train_labels), (
-        test_images, test_labels) = keras.datasets.mnist.load_data()
-        model = intro_keras.build_model()
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-        intro_keras.train_model(model, train_images, train_labels, 10)
-        output = capturedOutput.getvalue()
-        sys.stdout = sys.__stdout__
-
+        # Check layers
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            model.summary()
         expected_summary = [
-            "_________________________________________________________________",
-            "Layer (type)                 Output Shape              Param #   ",
-            "=================================================================",
-            "flatten_3 (Flatten)          (32, 784)                 0         ",
-            "_________________________________________________________________",
-            "dense_9 (Dense)              (32, 128)                 100480    ",
-            "_________________________________________________________________",
-            "dense_10 (Dense)             (32, 64)                  8256      ",
-            "_________________________________________________________________",
-            "dense_11 (Dense)             (32, 10)                  650       ",
-            "=================================================================",
-            "Total params: 109,386",
-            "Trainable params: 109,386",
-            "Non-trainable params: 0",
-            "_________________________________________________________________",
-            ""]
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-        model.summary()#prints to stdio
-        output = capturedOutput.getvalue()
-        sys.stdout = sys.__stdout__
+            'Model: "sequential"',
+            '_________________________________________________________________',
+            'Layer (type)                 Output Shape              Param #',
+            '=================================================================',
+            'flatten (Flatten)            (None, 784)               0',
+            '_________________________________________________________________',
+            'dense (Dense)                (None, 128)               100480',
+            '_________________________________________________________________',
+            'dense_1 (Dense)              (None, 64)                8256',
+            '_________________________________________________________________',
+            'dense_2 (Dense)              (None, 10)                650',
+            '=================================================================',
+            'Total params: 109,386',
+            'Trainable params: 109,386',
+            'Non-trainable params: 0',
+            '_________________________________________________________________'
+        ]
 
-        # remove model name because it's numbered
-        summary_lines = output.split('\n')[1:]
-
-        for i, (ref, real) in enumerate(zip(expected_summary, summary_lines)):
-            if i in (3,5,7,9):
-                # layer names are numbered according to the order
-                # you run the tests in
-                ref = "".join(ref.split(" ")[1:])
-                real = "".join(real.split(" ")[1:])
-            self.assertEqual(ref.rstrip(), real.rstrip())
+        summary_lines = captured_output.getvalue().split('\n')
+        for i in (4, 6, 8, 10):
+            # Layer names are numbered according to the order you run the tests,
+            # so we have to omit them from the comparison
+            real = ''.join(summary_lines[i].split(' ')[1:])
+            ref = ''.join(expected_summary[i].split(' ')[1:])
+            self.assertEqual(real, ref)
 
 
-    
+    def test_train_model(self):
+        train_images, train_labels = get_dataset()
+        model = build_model()
+
+        # Check that the number of samples is printed
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            train_model(model, train_images, train_labels, 10)
+        self.assertEqual(captured_output.getvalue().split('\n')[0],
+                'Train on 60000 samples')
+
+        # If this test fails, you can probably(?) comment it out
+        self.assertEqual(model.metrics_names, ['loss', 'accuracy'])
+
+
     def test_evaluate_model(self):
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
-        (train_images, train_labels), (
-        test_images, test_labels) = keras.datasets.mnist.load_data()
-        model = intro_keras.build_model()
-        intro_keras.train_model(model, train_images, train_labels, 10)
-        intro_keras.evaluate_model(model, test_images, test_labels)
-        output = capturedOutput.getvalue()
-        sys.stdout = sys.__stdout__
+        train_images, train_labels = get_dataset()
+        test_images, test_labels = get_dataset(False)
+        model = build_model()
+        with contextlib.redirect_stdout(io.StringIO()):
+            train_model(model, train_images, train_labels, 10)
 
-        # splice only the evaluate part
-        summary_lines = output.split('\n')[21:]
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            evaluate_model(model, test_images, test_labels)
+
+        # Splice only the evaluate part
+        summary_lines = captured_output.getvalue().split('\n')
+        # Check that nothing extra is printed (note that 3 is used here because
+        # the last newline character causes an empty string to be the last
+        # element of summary_lines)
+        self.assertEqual(len(summary_lines), 3,
+                'Printed incorrect number of lines')
+        self.assertEqual(summary_lines[-1], '')
+
         loss_line = summary_lines[0]
-        loss = float(loss_line.split(":")[1].strip())
-        self.assertAlmostEqual(loss, 0.2, delta=0.05)
-        accuracy_line = summary_lines[1]
-        accuracy = float(accuracy_line.split(":")[1].strip().strip('%'))
-        self.assertAlmostEqual(accuracy, 94.5, delta=1.0)
+        self.assertEqual(loss_line[:8], 'Loss: 0.')
+        self.assertEqual(len(loss_line), 12, 'Loss needs to have 4 decimals')
+        self.assertAlmostEqual(float(loss_line[6:]), 0.2, delta=0.05)
 
+        accuracy_line = summary_lines[1]
+        self.assertEqual(accuracy_line[:10], 'Accuracy: ')
+        self.assertEqual(accuracy_line[-1], '%')
+        self.assertEqual(len(accuracy_line), 16,
+                'Accuracy needs to have 2 decimals')
+        self.assertAlmostEqual(float(accuracy_line[10:15]), 94.5, delta=1.0)
+
+        # Repeat the test, but only print the accuracy
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            evaluate_model(model, test_images, test_labels, False)
+
+        summary_lines = captured_output.getvalue().split('\n')
+        self.assertEqual(len(summary_lines), 2,
+                'Printed incorrect number of lines')
+        self.assertEqual(summary_lines[-1], '')
+
+        accuracy_line = summary_lines[0]
+        self.assertEqual(accuracy_line[:10], 'Accuracy: ')
+        self.assertEqual(accuracy_line[-1], '%')
+        self.assertEqual(len(accuracy_line), 16,
+                'Accuracy needs to have 2 decimals')
+        self.assertAlmostEqual(float(accuracy_line[10:15]), 94.5, delta=1.0)
 
 
     def test_predict_label(self):
-        capturedOutput = io.StringIO()
-        sys.stdout = capturedOutput
+        train_images, train_labels = get_dataset()
+        test_images, test_labels = get_dataset(False)
+        model = build_model()
+        with contextlib.redirect_stdout(io.StringIO()):
+            train_model(model, train_images, train_labels, 10)
+            evaluate_model(model, test_images, test_labels)
 
-        (train_images, train_labels), (
-        test_images, test_labels) = keras.datasets.mnist.load_data()
-        model = intro_keras.build_model()
-        intro_keras.train_model(model, train_images, train_labels, 10)
-        intro_keras.evaluate_model(model, test_images, test_labels)
         model.add(keras.layers.Softmax())
-        intro_keras.predict_label(model, test_images, 1)
+        captured_output = io.StringIO()
+        with contextlib.redirect_stdout(captured_output):
+            predict_label(model, test_images, 1)
 
-        output = capturedOutput.getvalue().split('\n')
-        sys.stdout = sys.__stdout__
+        predictions = captured_output.getvalue().split('\n')
+        self.assertEqual(len(predictions), 4, 'Must print 3 predictions')
+        self.assertEqual(predictions[-1], '')
 
-        # splice out the non-prediction stuff
-        output = output[23:]
+        # Pick the piece of the output corresponding to the best prediction
+        best_prediction = predictions[0]
+        self.assertEqual(best_prediction[:5], 'Two: ')
+        self.assertEqual(best_prediction[-1], '%')
+        self.assertTrue(len(best_prediction) == 11
+                or best_prediction == 'Two: 100.00%',
+                'Prediction needs to have 2 decimals')
 
-        best_pred = output[0].split(':')[0]
-        self.assertEqual(best_pred, "Two")
 
-    
-if __name__ == "__main__":
+if __name__ == '__main__':
+    print('Homework 9 Tester Version', __version__)
     unittest.main()
